7dbace3b5db253e483eb8b9861b41c1728bc265d
Ariel <1rxf0@notsharingmy.info>
2013-07-28 15:57:29 -0700

    -Created the LQSGLProgram class

---
+++ Snake/LQSGLProgram.m
@@ -0,0 +1,13 @@
+//
+//  LQSGLProgram.m
+//  Snake
+//
+//  Created by Ariel on 2013-07-28.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import "LQSGLProgram.h"
+
+@implementation LQSGLProgram
+
+@end
