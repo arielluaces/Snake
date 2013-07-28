1a7e025f736155d6cd739e145e53f7c75015b556
Ariel <1rxf0@notsharingmy.info>
2013-07-28 15:59:45 -0700

    -Created the LQSGLFragmentShader class

---
+++ Snake/LQSGLFragmentShader.h
@@ -0,0 +1,13 @@
+//
+//  LQSGLFragmentShader.h
+//  Snake
+//
+//  Created by Ariel on 2013-07-28.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LQSGLFragmentShader : NSObject
+
+@end
---
+++ Snake/LQSGLFragmentShader.m
@@ -0,0 +1,13 @@
+//
+//  LQSGLFragmentShader.m
+//  Snake
+//
+//  Created by Ariel on 2013-07-28.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import "LQSGLFragmentShader.h"
+
+@implementation LQSGLFragmentShader
+
+@end
