e48adbcbb264138277a55f9b75ea7bda552f78c8
Ariel <1rxf0@notsharingmy.info>
2013-07-28 15:58:59 -0700

    -Created the LQSGLVertexShader class

---
+++ Snake/LQSGLVertexShader.m
@@ -0,0 +1,13 @@
+//
+//  LQSGLVertexShader.m
+//  Snake
+//
+//  Created by Ariel on 2013-07-28.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import "LQSGLVertexShader.h"
+
+@implementation LQSGLVertexShader
+
+@end
