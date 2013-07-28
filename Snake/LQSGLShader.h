0d7296e923b38379b3c13a871f8caaf33bf6610b
Ariel <1rxf0@notsharingmy.info>
2013-07-28 16:00:15 -0700

    -Created the LQSGLShader class

---
+++ Snake/LQSGLShader.h
@@ -0,0 +1,13 @@
+//
+//  LQSGLShader.h
+//  Snake
+//
+//  Created by Ariel on 2013-07-28.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LQSGLShader : NSObject
+
+@end
---
+++ Snake/LQSGLShader.m
@@ -0,0 +1,13 @@
+//
+//  LQSGLShader.m
+//  Snake
+//
+//  Created by Ariel on 2013-07-28.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import "LQSGLShader.h"
+
+@implementation LQSGLShader
+
+@end
