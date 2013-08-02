7230a2de6007c8758354d3b9923d6e55267887b8
Ariel <1rxf0@notsharingmy.info>
2013-08-01 21:12:23 -0700

    -Loading a GL program in the view controller

--- Snake/LQSViewController.m
+++ Snake/LQSViewController.m
@@ -7,9 +7,19 @@
 //
 
 #import "LQSViewController.h"
+#import "ILQSGLShader.h"
+#import "ILQSGLProgram.h"
+#import "LQSGLVertexShader.h"
+#import "LQSGLFragmentShader.h"
+#import "LQSGLProgram.h"
+#import "LQSGLUtils.h"
+#import <Foundation/NSBundle.h>
 
 @implementation LQSViewController
 {
+    NSObject<ILQSGLShader> *_vertexShader;
+    NSObject<ILQSGLShader> *_fragmentShader;
+    NSObject<ILQSGLProgram> *_program;
     EAGLContext *_context;
 }
 
@@ -24,6 +34,40 @@
     [EAGLContext setCurrentContext:_context];
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.65f, 0.65f, 0.65f, 1.0f);
+    {
+        // Create program
+        _vertexShader = [[LQSGLVertexShader alloc] initWithContext:_context];
+        {
+            NSStringEncoding *vertexShaderSourceEncoding = nil;
+            NSError *vertexShaderError;
+            NSString *vertexShaderFilePath = [[NSBundle mainBundle] pathForResource:@"Shader" ofType:@"vsh"];
+            NSString *vertexShaderSource = [NSString stringWithContentsOfFile:vertexShaderFilePath usedEncoding:vertexShaderSourceEncoding error:&vertexShaderError];
+            const GLchar *source = [vertexShaderSource UTF8String];
+            NSLog(@"%s",source);
+            glShaderSource(_vertexShader.name, 1, &source, NULL);
+            glCompileShader(_vertexShader.name);
+            [LQSGLUtils checkShaderCompileStatus:_vertexShader];
+        }
+        _fragmentShader = [[LQSGLFragmentShader alloc] initWithContext:_context];
+        {
+            NSStringEncoding *fragmentShaderSourceEncoding = nil;
+            NSError *fragmentShaderError;
+            NSString *fragmentShaderFilePath = [[NSBundle mainBundle] pathForResource:@"Shader" ofType:@"fsh"];
+            NSString *fragmentShaderSource = [NSString stringWithContentsOfFile:fragmentShaderFilePath usedEncoding:fragmentShaderSourceEncoding error:&fragmentShaderError];
+            const GLchar *source = [fragmentShaderSource UTF8String];
+            NSLog(@"%s",source);
+            glShaderSource(_fragmentShader.name, 1, &source, NULL);
+            glCompileShader(_fragmentShader.name);
+            [LQSGLUtils checkShaderCompileStatus:_fragmentShader];
+        }
+        _program = [[LQSGLProgram alloc] initWithContext:_context];
+        {
+            glAttachShader(_program.name, _vertexShader.name);
+            glAttachShader(_program.name, _fragmentShader.name);
+            glLinkProgram(_program.name);
+            [LQSGLUtils checkProgramLinkStatus:_program];
+        }
+    }
     [EAGLContext setCurrentContext:savedContext];
 }
 
