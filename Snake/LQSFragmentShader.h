dc54bf189d7740c686c4bb516987d3a7e896d4ad
Ariel <1rxf0@notsharingmy.info>
2013-08-02 18:55:09 -0700

    -Created a fragment shader class that can allocate and compile GL a fragment when given the source code

---
+++ Snake/LQSFragmentShader.h
@@ -0,0 +1,19 @@
+//
+//  LQSFragmentShader.h
+//  Snake
+//
+//  Created by Ariel on 2013-08-02.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OpenGLES/EAGL.h>
+#import "ILQSGLShader.h"
+
+@interface LQSFragmentShader : NSObject <ILQSGLShader>
+
+- (id)initWithSource:(const char*)source;
+- (id)initWithSource:(const char*)source context:(EAGLContext *)context;
+- (id)initWithSource:(const char*)source sharegroup:(EAGLSharegroup *)sharegroup;
+
+@end
