2eb3b3a854cb89c0917b9b6152a95feac72167f1
Ariel <1rxf0@notsharingmy.info>
2013-09-01 17:14:43 -0700

    -Basic concepts of transformation matrices implemented

--- Snake/LQSViewController.m
+++ Snake/LQSViewController.m
@@ -27,6 +27,11 @@
     float _exponent;
     
     NSObject<ILQSColoredVerticesProgram> *_program2;
+    float _squareSpaceToRootSpaceScale;
+    NSObject *_squareSpace;
+    NSObject *_squareSpaceParent;
+    NSObject *_squareSpaceToParentTransform;
+    NSObject *_rootSpace;
 }
 
 - (void)viewDidLoad
@@ -38,6 +43,12 @@
     self.glkView.drawableDepthFormat = GLKViewDrawableDepthFormat24;
     EAGLContext *savedContext = [EAGLContext currentContext];
     [EAGLContext setCurrentContext:_context];
+    // Create space information for the square being drawn
+    _squareSpace = [[NSObject alloc] init];
+    _rootSpace = [[NSObject alloc] init];
+    _squareSpaceParent = _rootSpace;
+    _squareSpaceToParentTransform = _squareSpace;
+    _squareSpaceToRootSpaceScale = 1.0f/16.0f;
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     {
         // Create program
@@ -85,6 +96,147 @@
     _exponent = _exponent+1.0f;
 }
 
+- (GLKMatrix4)transformationMatrixFromSpace:(NSObject *)space1 toSpace:(NSObject *)space2
+{
+    if (space1 == space2)
+    {
+        return GLKMatrix4Identity;
+    }
+    else if ([self isObject:space2 InAdjacentObjectsOfObject:space1])
+    {
+        return [self transformationMatrixOfSpace:space1 toSpace:space2];
+    }
+    else if ([self isObject:space1 InAdjacentObjectsOfObject:space2])
+    {
+        // We can either ask the object for the matrix already ionverted or we could invert the matrix ourselves
+        // The matrices can be inverted individually or a large set of matrices that need to be inverted can
+        // be inverted one chunk after being multiplied together
+        return [self transformationMatrixInverseOfSpace:space2 toSpace:space1];
+    }
+    else
+    {
+        // Start looking for longer chains in the directed graph to end up with a path from space1 to space2
+        NSAssert(FALSE, @"Not implemented yet");
+        return GLKMatrix4Identity;
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return GLKMatrix4Identity;
+}
+
+- (GLKMatrix4)transformationMatrixOfSpace:(NSObject *)space1 toSpace:(NSObject *)space2
+{
+    if (space1 == _squareSpace)
+    {
+        if (space2 == _squareSpaceParent)
+        {
+            return [self transformatrionMatrixOfObject:[self transformObjectOfOBject:space1]];
+        }
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return GLKMatrix4Identity;
+}
+
+- (GLKMatrix4)transformationMatrixInverseOfSpace:(NSObject *)space1 toSpace:(NSObject *)space2
+{
+    if (space1 == _squareSpace)
+    {
+        if (space2 == _squareSpaceParent)
+        {
+            return [self transformatrionMatrixInverseOfObject:[self transformObjectOfOBject:space1]];
+        }
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return GLKMatrix4Identity;
+}
+
+- (bool)isChild:(NSObject *)object
+{
+    if (object == _squareSpace)
+    {
+        return true;
+    }
+    else if (object == _rootSpace)
+    {
+        return false;
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return false;
+}
+
+- (bool)isObject:(NSObject *)adjacentObject InAdjacentObjectsOfObject:(NSObject *)object
+{
+    // Should be equivalent to "adjacentObject" being in the set of adjacent objects of "object"
+    // This function is just used of optimizations so that the adjacnet objects set doesn't need
+    // to be constructed in order to determine if an "adjacentObject" is in the set
+    if (object == _squareSpace)
+    {
+        if (adjacentObject == _squareSpaceParent)
+        {
+            return true;
+        }
+    }
+    else if (object == _rootSpace)
+    {
+        return false;
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return false;
+}
+
+- (NSArray *)adjacentSpacesOfObject:(NSObject *)object
+{
+    if (object == _squareSpace)
+    {
+        return [NSArray arrayWithObject:_squareSpaceParent];
+    }
+    else if (object == _rootSpace)
+    {
+        return [NSArray array];
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return [NSArray array];
+}
+
+- (NSObject *)transformObjectOfOBject:(NSObject *)object
+{
+    if (object == _squareSpace)
+    {
+        return _squareSpaceToParentTransform;
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return nil;
+}
+
+- (NSObject *)parentOfObject:(NSObject *)object
+{
+    if (object == _squareSpace)
+    {
+        return _squareSpaceParent;
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return nil;
+}
+
+- (GLKMatrix4)transformatrionMatrixOfObject:(NSObject *)object;
+{
+    if (object == _squareSpaceToParentTransform)
+    {
+        return GLKMatrix4MakeScale(_squareSpaceToRootSpaceScale, _squareSpaceToRootSpaceScale, _squareSpaceToRootSpaceScale);
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return GLKMatrix4Identity;
+}
+
+- (GLKMatrix4)transformatrionMatrixInverseOfObject:(NSObject *)object
+{
+    if (object == _squareSpaceToParentTransform)
+    {
+        return GLKMatrix4MakeScale(1.0f/_squareSpaceToRootSpaceScale, 1.0f/_squareSpaceToRootSpaceScale, 1.0f/_squareSpaceToRootSpaceScale);
+    }
+    NSAssert(FALSE, @"This is not suppoed to run");
+    return GLKMatrix4Identity;
+}
+
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -127,7 +279,8 @@
             1.0f, 0.0f,
             1.0f, 1.0f,
         };
-        glUniformMatrix4fv(_program2.uMVPMatrix, 1, GL_FALSE, GLKMatrix4MakeScale(1.0f/16.0f, 1.0f/16.0f, 1.0f/16.0f).m);
+        GLKMatrix4 MVPMatrix = [self transformationMatrixFromSpace:_squareSpace toSpace:_rootSpace];
+        glUniformMatrix4fv(_program2.uMVPMatrix, 1, GL_FALSE, MVPMatrix.m);
         glVertexAttribPointer(_program2.aPosition, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, vertexPositions);
         glUniform3f(_program2.uColor, 0.6f, 0.2f, 0.95f);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
