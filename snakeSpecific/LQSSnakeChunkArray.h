ff72439cd4d6f3fbd00bf96c9b6f5f042cc50510
Ariel <1rxf0@notsharingmy.info>
2013-12-18 20:36:41 -0800

    -Created a snake chunk array through the use of the generic array macro files

---
+++ snakeSpecific/LQSSnakeChunkArray.h
@@ -0,0 +1,11 @@
+//
+//  LQSSnakeChunkArray.h
+//  Snake
+//
+//  Created by Ariel School on 2013-12-16.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#define __LQS_GENERIC_NAME__ SnakeChunk
+#import "LQSGenericArray.h"
+#undef __LQS_GENERIC_NAME__
