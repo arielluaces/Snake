eeb8a400dbf0a07f04866a4453cbff0a4ecc4c8a
Ariel <1rxf0@notsharingmy.info>
2013-12-29 16:08:17 -0800

    -Created a dummy touch processor

---
+++ touch/LQSTouchInputState.h
@@ -0,0 +1,13 @@
+//
+//  LQSTouchInputState.h
+//  Snake
+//
+//  Created by Ariel School on 2013-12-29.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import "ILQSTouchProcessor.h"
+
+@interface LQSTouchInputState : NSObject<ILQSTouchProcessor>
+
+@end
---
+++ touch/LQSTouchInputState.m
@@ -0,0 +1,19 @@
+//
+//  LQSTouchInputState.m
+//  Snake
+//
+//  Created by Ariel School on 2013-12-29.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import "LQSTouchInputState.h"
+#import <UIKit/UITouch.h>
+
+@implementation LQSTouchInputState
+
+- (void)processTouch:(UITouch *)touch
+{
+    
+}
+
+@end
