788cc3b97ca05a5c2b810d82e1bd530e0ac3be86
Ariel <1rxf0@notsharingmy.info>
2013-12-29 18:37:19 -0800

    -Created an updatable interface which can be used by screipts in the game to update themselves

---
+++ snakeSpecific/ILQSUpdatable.h
@@ -0,0 +1,15 @@
+//
+//  ILQSUpdatable.h
+//  Snake
+//
+//  Created by Ariel School on 2013-12-29.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ILQSUpdatable <NSObject>
+
+- (void)update;
+
+@end
