c6dada311a1812a98f212f579672af8a3273e0f9
Ariel <1rxf0@notsharingmy.info>
2013-12-29 18:39:47 -0800

    -Created an array of updatable objects

---
+++ snakeSpecific/LQSUpdatableArray.h
@@ -0,0 +1,11 @@
+//
+//  LQSUpdatableArray.h
+//  Snake
+//
+//  Created by Ariel School on 2013-12-29.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#define __LQS_GENERIC_NAME__ Updatable
+#import "LQSGenericArray.h"
+#undef __LQS_GENERIC_NAME__
---
+++ snakeSpecific/LQSUpdatableArray.m
@@ -0,0 +1,11 @@
+//
+//  LQSUpdatableArray.m
+//  Snake
+//
+//  Created by Ariel School on 2013-12-29.
+//  Copyright (c) 2013 Liquid Sparks. All rights reserved.
+//
+
+#define __LQS_GENERIC_NAME__ Updatable
+#import "LQSGenericArray.m"
+#undef __LQS_GENERIC_NAME__
