e4bfc49c0fe1aa772d43ff60ae3ddd7cb2f7b32d
Ariel <1rxf0@notsharingmy.info>
2014-01-05 14:40:50 -0800

    -Created a test class for testing scale transformations

---
+++ transformationsTests/LQSScaleTransformationTests.m
@@ -0,0 +1,34 @@
+//
+//  LQSScaleTransformationTests.m
+//  Snake
+//
+//  Created by Ariel School on 2014-01-05.
+//  Copyright (c) 2014 Liquid Sparks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LQSScaleTransformationTests : SenTestCase
+
+@end
+
+@implementation LQSScaleTransformationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
