ffdb76edfeb62598c186ff6c838d4ba26c912663
Ariel <1rxf0@notsharingmy.info>
2014-01-05 14:19:46 -0800

    -Created a test class for testing rotation transformations

---
+++ transformationsTests/LQSRotationTransformationTests.m
@@ -0,0 +1,34 @@
+//
+//  LQSRotationTransformationTests.m
+//  Snake
+//
+//  Created by Ariel School on 2014-01-05.
+//  Copyright (c) 2014 Liquid Sparks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LQSRotationTransformationTests : SenTestCase
+
+@end
+
+@implementation LQSRotationTransformationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
