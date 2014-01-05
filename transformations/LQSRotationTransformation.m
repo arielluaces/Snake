4f6af978102bc085e822bd23bd0230349c9e9b25
Ariel <1rxf0@notsharingmy.info>
2014-01-05 14:38:09 -0800

    -Created a test for the situation where a rotation transformation has its x,y,z components all set to zero -Created an assert in the rotation tranformation objects to check for x,y,z components all being set to zero

--- transformations/LQSRotationTransformation.m
+++ transformations/LQSRotationTransformation.m
@@ -13,11 +13,13 @@
 
 - (GLKMatrix4)transformationMatrix
 {
+    NSAssert(_x!=0 && _y!=0 && _z!=0, @"The x,y, and z components of this rotation transformation can't all be zero");
     return GLKMatrix4MakeRotation(_radians, _x, _y, _z);
 }
 
 - (GLKMatrix4)transformationMatrixInverse
 {
+    NSAssert(_x!=0 && _y!=0 && _z!=0, @"The x,y, and z components of this rotation transformation can't all be zero");
     return GLKMatrix4MakeRotation(-_radians, _x, _y, _z);
 }
 
--- transformationsTests/LQSRotationTransformationTests.m
+++ transformationsTests/LQSRotationTransformationTests.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Liquid Sparks. All rights reserved.
 //
 
+#import "LQSRotationTransformation.h"
 #import <SenTestingKit/SenTestingKit.h>
 
 @interface LQSRotationTransformationTests : SenTestCase
@@ -29,6 +30,35 @@
 - (void)testExample
 {
     STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testZeroVectorRotation
+{
+    LQSRotationTransformation *rotationTransformation = [[LQSRotationTransformation alloc] init];
+    {
+        rotationTransformation.radians = 0;
+        rotationTransformation.x = 0;
+        rotationTransformation.y = 0;
+        rotationTransformation.z = 0;
+        STAssertThrows(rotationTransformation.transformationMatrix, @"The rotation transformation shouldn't be producing transformation matrices when the x,y,z components are all zero");
+        STAssertThrows(rotationTransformation.transformationMatrixInverse, @"The rotation transformation shouldn't be producing transformation matrices when the x,y,z components are all zero");
+    }
+    {
+        rotationTransformation.radians = 100;
+        rotationTransformation.x = 0;
+        rotationTransformation.y = 0;
+        rotationTransformation.z = 0;
+        STAssertThrows(rotationTransformation.transformationMatrix, @"The rotation transformation shouldn't be producing transformation matrices when the x,y,z components are all zero");
+        STAssertThrows(rotationTransformation.transformationMatrixInverse, @"The rotation transformation shouldn't be producing transformation matrices when the x,y,z components are all zero");
+    }
+    {
+        rotationTransformation.radians = -100;
+        rotationTransformation.x = 0;
+        rotationTransformation.y = 0;
+        rotationTransformation.z = 0;
+        STAssertThrows(rotationTransformation.transformationMatrix, @"The rotation transformation shouldn't be producing transformation matrices when the x,y,z components are all zero");
+        STAssertThrows(rotationTransformation.transformationMatrixInverse, @"The rotation transformation shouldn't be producing transformation matrices when the x,y,z components are all zero");
+    }
 }
 
 @end
