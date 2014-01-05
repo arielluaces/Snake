cca0469a75ab2bdef0730c05b1a9e51e496ba2b7
Ariel <1rxf0@notsharingmy.info>
2014-01-05 15:50:27 -0800

    -Created some code for testing the scenario where at least one of the components of a scale transformation is set to zero, causing the resulting inverse transformation to be undefined

--- transformationsTests/LQSScaleTransformationTests.m
+++ transformationsTests/LQSScaleTransformationTests.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Liquid Sparks. All rights reserved.
 //
 
+#import "LQSScaleTransformation.h"
 #import <SenTestingKit/SenTestingKit.h>
 
 @interface LQSScaleTransformationTests : SenTestCase
@@ -29,6 +30,59 @@
 - (void)testExample
 {
     STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testZeroScaleTransformations
+{
+    {
+        LQSScaleTransformation *scaleTransormation = [[LQSScaleTransformation alloc] init];
+        scaleTransormation.scaleX = 1;
+        scaleTransormation.scaleY = 1;
+        scaleTransormation.scaleZ = 1;
+        STAssertNoThrow(scaleTransormation.transformationMatrixInverse, @"The scale transformation should be producing inverse transformation matrices under normal circumstances");
+    }
+    {
+        LQSScaleTransformation *scaleTransormation = [[LQSScaleTransformation alloc] init];
+        scaleTransormation.scaleX = 0;
+        scaleTransormation.scaleY = 1;
+        scaleTransormation.scaleZ = 1;
+        STAssertThrows(scaleTransormation.transformationMatrixInverse, @"The scale transformation shouldn't be producing inverse transformation matrices when the x component is zero");
+    }
+    {
+        LQSScaleTransformation *scaleTransormation = [[LQSScaleTransformation alloc] init];
+        scaleTransormation.scaleX = 1;
+        scaleTransormation.scaleY = 0;
+        scaleTransormation.scaleZ = 1;
+        STAssertThrows(scaleTransormation.transformationMatrixInverse, @"The scale transformation shouldn't be producing inverse transformation matrices when the y component is zero");
+    }
+    {
+        LQSScaleTransformation *scaleTransormation = [[LQSScaleTransformation alloc] init];
+        scaleTransormation.scaleX = 1;
+        scaleTransormation.scaleY = 1;
+        scaleTransormation.scaleZ = 0;
+        STAssertThrows(scaleTransormation.transformationMatrixInverse, @"The scale transformation shouldn't be producing inverse transformation matrices when the z component is zero");
+    }
+    {
+        LQSScaleTransformation *scaleTransormation = [[LQSScaleTransformation alloc] init];
+        scaleTransormation.scaleX = 0;
+        scaleTransormation.scaleY = 0;
+        scaleTransormation.scaleZ = 1;
+        STAssertThrows(scaleTransormation.transformationMatrixInverse, @"The scale transformation shouldn't be producing inverse transformation matrices when the x and y components are zero");
+    }
+    {
+        LQSScaleTransformation *scaleTransormation = [[LQSScaleTransformation alloc] init];
+        scaleTransormation.scaleX = 0;
+        scaleTransormation.scaleY = 1;
+        scaleTransormation.scaleZ = 0;
+        STAssertThrows(scaleTransormation.transformationMatrixInverse, @"The scale transformation shouldn't be producing inverse transformation matrices when the x and z components are zero");
+    }
+    {
+        LQSScaleTransformation *scaleTransormation = [[LQSScaleTransformation alloc] init];
+        scaleTransormation.scaleX = 0;
+        scaleTransormation.scaleY = 0;
+        scaleTransormation.scaleZ = 0;
+        STAssertThrows(scaleTransormation.transformationMatrixInverse, @"The scale transformation shouldn't be producing inverse transformation matrices when the x, y, and z components are zero");
+    }
 }
 
 @end
