5c6a4e27e57730724f5e20a1497cb278afdbb25c
Ariel <1rxf0@notsharingmy.info>
2014-01-05 16:38:49 -0800

    -Created a test class for testing uniform scale transformations

---
+++ transformationsTests/LQSUniformScaleTransformationTests.m
@@ -0,0 +1,34 @@
+//
+//  LQSUniformScaleTransformationTests.m
+//  Snake
+//
+//  Created by Ariel School on 2014-01-05.
+//  Copyright (c) 2014 Liquid Sparks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LQSUniformScaleTransformationTests : SenTestCase
+
+@end
+
+@implementation LQSUniformScaleTransformationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
